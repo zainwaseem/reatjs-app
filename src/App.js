c8d578739ee7c15a77a052f36ee00ba3050f1f9f
zainwaseem <zainwaseem9371@gmail.com>
2025-06-23 19:35:52 +0500

    ENV FILE

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Zaid Github Actions</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Student Info</h1>
+        <p>
+          <strong>Name:</strong> {process.env.REACT_APP_NAME}
+        </p>
+        <p>
+          <strong>Roll Number:</strong> {process.env.REACT_APP_ROLL_NUMBER}
+        </p>
+        <p>
+          <strong>Class:</strong> {process.env.REACT_APP_CLASS}
+        </p>
+        <p>
+          <strong>About Me:</strong> {process.env.REACT_APP_BIO}
+        </p>
       </header>
     </div>
   );
